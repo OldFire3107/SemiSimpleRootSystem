43ad744bde40637d5fd60d74e445c3a09b0515e8
Agni Purani <agni.purani@gmail.com>
2022-07-12 01:07:03 +0200

    Made sympy to display roots

--- Semisimple/RootSystem.py
+++ Semisimple/RootSystem.py
@@ -1,5 +1,4 @@
-from cmath import isclose
-from matplotlib.pyplot import axis
+from sympy import symbols
 import numpy as np
 
 class RootSystem:
@@ -15,48 +14,86 @@ class RootSystem:
         assert rootlist is not None, 'Provide at least one root!'
         rootlist = np.array(rootlist)
         length = len(rootlist[0])
-        dim = len(rootlist)
+        self.dim = len(rootlist)
+        self.rootsyms = symbols(f'alpha1:{self.dim+1}', commutative=False)
         for simpleroot in rootlist[1:]:
             assert len(simpleroot) == length , 'Root vector sizes inconsistent!'
 
-        assert np.linalg.matrix_rank(rootlist) == dim, 'Simple Roots provided are not linearly independant!'
+        assert np.linalg.matrix_rank(rootlist) == self.dim, 'Simple Roots provided are not linearly independant!'
 
         self.simprootlist = rootlist
+        self.gen_adj_list_and_cartan_mat()
 
-        # Getting the adjecency list
+    def gen_adj_list_and_cartan_mat(self):
+        '''
+        Computes and Returns the adjecency list of the system and its Cartan Matrix.
+        '''
+
         self.adj_list = {}
-        self.cartan_matrix = np.zeros((dim,dim), dtype=int)
+        self.cartan_matrix = np.zeros((self.dim, self.dim), dtype=int)
         for i in range(len(self.simprootlist)):
             simple_root = self.simprootlist[i]
             result = 2. * np.dot(self.simprootlist, simple_root) / (np.linalg.norm(simple_root) ** 2)
             resultint = np.array(result, dtype=int)
+            self.cartan_matrix[:,i] =  resultint
             assert np.isclose(result, resultint).all(), 'Invalid angle between the roots!'
-            print(resultint)
             list_node = np.where(np.logical_not(np.logical_or(resultint == 0, resultint == 2)))
             self.adj_list[i] = list_node
 
-        print(self.adj_list)
+        ## Checking it angles are valid.
+        diag2 = np.diag(np.diag(self.cartan_matrix))
+        check_mat = self.cartan_matrix - diag2
+        num_list = np.unique(check_mat)
+        for i in range(len(num_list)):
+            assert num_list[i] in [0, -1, -2, -3], 'Invalid angle between the roots!'
+
+        ## divided by right side and multiplied by 2
+        self.replacements = [(self.rootsyms[i] * self.rootsyms[j], self.cartan_matrix[i][j]) for i in range(self.dim) for j in range(self.dim)]
+
+        return self.adj_list, self.cartan_matrix
+
+    def get_positive_roots_layered(self):
+        '''
+        Returns an array of all roots generated from the simple roots.
+        '''
+        PositiveRoots = []
+
+        # Starting with the i th row of the cartan matrix as alpha_i
+        # k = 0 is just the cartan matrix
+        # k = 1 layer will always be there
+        kzero = []
+
+        for simpleroots in self.rootsyms:
+            kzero.append(simpleroots)
         
+        PositiveRoots.append(kzero)
+        
+        # Now for values k > 1
+        k_val = 1
+        Possible = True
+        while Possible:
+            Possible = False
+            k_val += 1
+            krowlist = []
+            # Get the value of p associate with the k value
+            for i in range(self.dim):
+                for j in range(self.dim):
+                    qpval = PositiveRoots[k_val-2][i] * self.rootsyms[j]
+                    # We take the cartan matrix values, so the division by self.rootsyms[j]
+                    # square is not necessary.
+                    qpval = qpval.expand().subs(self.replacements)
+                    print(qpval)
+
+            
+
+        return PositiveRoots
+
+    def generate_simple_roots(self):
+        '''
+        Returns and generates simples roots from the Cartan Matrix in eucleadian geometry Vector Space.
+        '''
+
+        # ToDo
         
 
-        
-    def get_simple_roots(self):
-        '''
-        Returns the simple roots as an array of row vectors.
-        '''
-
-        return self.simprootlist
-
-    def get_cartan_matrix(self):
-        '''
-        Returns the cartan matrix of the system.
-        '''
-
-        return self.cartan_matrix
-
-    def get_adj_list(self):
-        '''
-        Returns the adjecency list of the system.
-        '''
-
-        return self.adj_list
+        pass
