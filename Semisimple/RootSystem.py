04918a3406c67eaf89a4c56c7684e12ea6794d5f
Agni Purani <agni.purani@gmail.com>
2022-07-20 18:01:55 +0530

    All roots generated from simple

--- Semisimple/RootSystem.py
+++ Semisimple/RootSystem.py
@@ -1,5 +1,8 @@
 from sympy import symbols
 import numpy as np
+import itertools
+
+from traitlets import All
 
 class RootSystem:
     '''
@@ -8,21 +11,59 @@ class RootSystem:
 
     The master class that does operations related to the entire system
     of roots based on provided simple roots.
+
+    - Use fromCartanMatrix Method if needed to be initialized from Cartan Matrix
+    - Use fromRootList if needed to be initialized from a root vector provided 
+      as in eucleadian vector space
     '''
 
-    def __init__(self, rootlist=None):
-        assert rootlist is not None, 'Provide at least one root!'
-        rootlist = np.array(rootlist)
-        length = len(rootlist[0])
-        self.dim = len(rootlist)
-        self.rootsyms = symbols(f'alpha1:{self.dim+1}', commutative=False)
-        for simpleroot in rootlist[1:]:
-            assert len(simpleroot) == length , 'Root vector sizes inconsistent!'
+    def __init__(self, rootlist=None, cart_mat=None):
 
-        assert np.linalg.matrix_rank(rootlist) == self.dim, 'Simple Roots provided are not linearly independant!'
+        if rootlist is not None:
+            rootlist = np.array(rootlist)
+            length = len(rootlist[0])
+            self.dim = len(rootlist)
+            self.rootsyms = symbols(f'alpha1:{self.dim+1}', commutative=False)
+            for simpleroot in rootlist[1:]:
+                assert len(simpleroot) == length , 'Root vector sizes inconsistent!'
 
-        self.simprootlist = rootlist
-        self.gen_adj_list_and_cartan_mat()
+            assert np.linalg.matrix_rank(rootlist) == self.dim, 'Simple Roots provided are not linearly independant!'
+
+            self.simprootlist = rootlist
+            self.gen_adj_list_and_cartan_mat()
+
+        else:
+            self.cartan_matrix = cart_mat
+            self.dim = len(cart_mat)
+            self.rootsyms = symbols(f'alpha1:{self.dim+1}', commutative=False)
+
+    @classmethod
+    def fromRootList(cls, RootList):
+        '''
+        Initialize the class from the Eucleadian Vector Space Roots
+        '''
+
+        assert RootList is not None, 'Provide at least one root!'
+        return cls(RootList, None)
+
+    @classmethod
+    def fromCartanMatrix(cls, CartMat):
+        '''
+        Initialize the class from the Cartan Matrix
+        '''
+        assert CartMat is not None, 'Provide a non empty Cartan Matrix!'
+        
+        ## Checking it angles are valid.
+        diag = np.diag(CartMat)
+        diaglist = np.unique(diag)
+        assert diaglist[0] == 2 and diaglist.size == 1, 'Invalid Cartan Matrix'
+        diag2 = np.diag(diag)
+        check_mat = CartMat - diag2
+        num_list = np.unique(check_mat)
+        for i in range(len(num_list)):
+            assert num_list[i] in [0, -1, -2, -3], 'Invalid angle between the roots!'
+
+        return cls(None, CartMat)
 
     def gen_adj_list_and_cartan_mat(self):
         '''
@@ -67,26 +108,82 @@ class RootSystem:
             kzero.append(simpleroots)
         
         PositiveRoots.append(kzero)
-        
-        # Now for values k > 1
+        # for values k > 1
+        # Now 
+        q_vals = [np.zeros(self.dim, dtype=int) for i in range(self.dim)]
+        for i in range(self.dim):
+            q_vals[i][i] = 2
+        q_vals = np.array(q_vals, dtype=int)
         k_val = 1
+
+        base_qp = []
+        # Get the q-p vals for all k=1 layer roots.
+        for i in range(self.dim):
+            qp_row = []
+            for j in range(self.dim):
+                qpval = PositiveRoots[k_val-2][i] * self.rootsyms[j]
+                # We take the cartan matrix values, so the division by self.rootsyms[j]
+                # square is not necessary.
+                qpval = qpval.expand().subs(self.replacements)
+                qp_row.append(qpval)
+
+            base_qp.append(qp_row)
+        base_qp = np.array(base_qp, dtype=int)
+        qpval = base_qp
+
         Possible = True
         while Possible:
             Possible = False
+            new_qvals = []
+            new_qpvals = []
             k_val += 1
             krowlist = []
-            # Get the value of p associate with the k value
-            for i in range(self.dim):
-                for j in range(self.dim):
-                    qpval = PositiveRoots[k_val-2][i] * self.rootsyms[j]
-                    # We take the cartan matrix values, so the division by self.rootsyms[j]
-                    # square is not necessary.
-                    qpval = qpval.expand().subs(self.replacements)
-                    print(qpval)
 
+            p_vals = q_vals - qpval
+            for i in range(len(p_vals)):
+                for j in range(self.dim):
+                    if p_vals[i][j] > 0:
+                        Possible = True
+                        newroot = PositiveRoots[k_val-2][i] + self.rootsyms[j]
+                        newqp_val = qpval[i] + base_qp[j]
+
+                        inList = False
+
+                        for no in range(len(krowlist)):
+                            if newroot == krowlist[no]:
+                                inList = True
+                                new_qvals[no][j] += 1
+
+                        if not inList:
+                            inList = True
+                            krowlist.append(newroot)
+                            new_qpvals.append(newqp_val)
+                            qval = []
+                            for k in range(self.dim):
+                                if k == j:
+                                    qval.append(q_vals[i][k] + 1)
+                                else:
+                                    qval.append(0)
+                            
+                            new_qvals.append(qval)
+            
+            if len(krowlist) == 0:
+                break
+
+            PositiveRoots.append(krowlist)
+            q_vals = np.array(new_qvals, dtype=int)
+            qpval = np.array(new_qpvals, dtype=int)
             
 
         return PositiveRoots
+
+    def get_all_roots(self):
+        Positive_roots = self.get_positive_roots_layered()
+        All_roots = list(itertools.chain(*Positive_roots))
+        for i in range(len(All_roots)):
+            nege = All_roots[i]*-1
+            All_roots.append(nege.expand())
+        return All_roots
 
     def generate_simple_roots(self):
         '''
