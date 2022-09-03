1c919f4be81d4222a1f2b397b265200a3d0e6b7e
Agni Purani <agni.purani@gmail.com>
2022-09-04 04:07:27 +0530

    Mind needs restructuring

--- Semisimple/TheAlgebra.py
+++ Semisimple/TheAlgebra.py
@@ -44,14 +44,21 @@ by initializing it or at a later stage using set_root_sys method\n\n')
             for j in range(len(self.rootsys.PositiveRoots[i])):
                 for k in range(i+1):
                     for l in range(j+1):
-                        if i == k and l == j:
-                            continue
                         
                         # New root with both positve
                         newrootpospos = self.rootsys.PositiveRoots[i][j] + self.rootsys.PositiveRoots[k][l]
                         newrootposneg = self.rootsys.PositiveRoots[i][j] - self.rootsys.PositiveRoots[k][l]
 
-                        # Computing for positive positive first
+                        if i == k and l == j:
+                            # For the pos neg case the end result is alpha dot H
+                            # They obivously commute as they are the same terms
+                            continue
+
+                        # Computing for positive negetive first
+                        if newrootposneg in self.rootsys.roots:
+                            mask = self.rootsys.q[i][j] != 0
+                            roots_to_switch = np.where(mask)[0]
+
                         if newrootpospos in self.rootsys.roots:
                             struct_constant = 0
 
@@ -63,35 +70,41 @@ by initializing it or at a later stage using set_root_sys method\n\n')
 
                                 common = list(set(roots_to_switch).intersection(roots_to_exchagne))
 
-                                if common is not None:
+                                if common:
                                     common = common[0]
                                 else:
                                     common = roots_to_switch[0]
                                     print("Didn't work out!!\n\n\n\n\n") # Debugging statement 
-
                                 
                                 subcommutator_term = self.rootsys.PositiveRoots[i][j] - self.rootsys.rootsyms[common]
-                                index0 = self.rootsys.roots.index(subcommutator_term + self.rootsys.PositiveRoots[k][l])
 
                                 index1 = self.rootsys.roots.index(subcommutator_term)
                                 index2 = self.rootsys.roots.index(self.rootsys.PositiveRoots[k][l])
+                                # index3 = self.rootsys.roots.index(self.rootsys.PositiveRoots[i][j])
 
-                                struct_constant = self.structure_constants[index1][index2][index0]
+                                # struct_constant = 1 / self.structure_constants[common][index1][index3]
 
-                                prefinal = subcommutator_term + self.rootsys.PositiveRoots[k][l]
-                                secondaryindex = self.rootsys.PositiveRoots[i+k].index(prefinal)
+                                try:
+                                    index0 = self.rootsys.roots.index(subcommutator_term + self.rootsys.PositiveRoots[k][l])
 
-                                j_val = (self.rootsys.q[i+k][secondaryindex][common] + self.rootsys.p[i+k][secondaryindex][common])/2
-                                m_val = (self.rootsys.q[i+k][secondaryindex][common] - self.rootsys.p[i+k][secondaryindex][common])/2
+                                    struct_constant = self.structure_constants[index1][index2][index0] * struct_constant
 
-                                struct_constant = struct_constant * np.sqrt((j_val + m_val + 1) * (j_val - m_val) / 2) / self.raising_norm[common]
+                                    prefinal = subcommutator_term + self.rootsys.PositiveRoots[k][l]
+                                    secondaryindex = self.rootsys.PositiveRoots[i+k].index(prefinal)
+
+                                    j_val = (self.rootsys.q[i+k][secondaryindex][common] + self.rootsys.p[i+k][secondaryindex][common])/2
+                                    m_val = (self.rootsys.q[i+k][secondaryindex][common] - self.rootsys.p[i+k][secondaryindex][common])/2
+
+                                    struct_constant = struct_constant * np.sqrt((j_val + m_val + 1) * (j_val - m_val) / 2) * self.raising_norm[common]
+                                except ValueError:
+                                    struct_constant = 0
 
                             else:
                                 common = self.rootsys.rootsyms.index(self.rootsys.PositiveRoots[k][l])
                                 j_val = (self.rootsys.q[i][j][common] + self.rootsys.p[i][j][common])/2
                                 m_val = (self.rootsys.q[i][j][common] - self.rootsys.p[i][j][common])/2
                                 
-                                struct_constant =  np.sqrt((j_val + m_val + 1) * (j_val - m_val) / 2) / self.raising_norm[common]
+                                struct_constant =  struct_constant * np.sqrt((j_val + m_val + 1) * (j_val - m_val) / 2) * self.raising_norm[common]
                             
                             a = self.rootsys.roots.index(self.rootsys.PositiveRoots[i][j])
                             b = self.rootsys.roots.index(self.rootsys.PositiveRoots[k][l])
@@ -108,12 +121,6 @@ by initializing it or at a later stage using set_root_sys method\n\n')
                           
                             self.structure_constants[a][b][c] = self.structure_constants[b][c][a] = self.structure_constants[c][a][b] = struct_constant
                             self.structure_constants[b][a][c] = self.structure_constants[a][c][b] = self.structure_constants[c][b][a] = -struct_constant
-
-
-                        # Computing the negetive
-                        if newrootposneg in self.rootsys.roots:
-                            pass
-
 
 
         
